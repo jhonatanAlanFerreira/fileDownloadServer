6757585a20314f5f89fa8cfdc3a8947c58757090
jhonatan <jhonatan.alanf@gmail.com>
2019-10-13 16:40:43 -0300

    workin

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,5 +1,11 @@
 module.exports = {
     async store(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
          let file = req.file.buffer;
 
          var fileContents = Buffer.from(file, "base64");
--- src/server.js
+++ src/server.js
@@ -8,11 +8,6 @@ mongoose.connect('mongodb+srv://jhon:140823allan@newocrapp-pbutv.mongodb.net/tes
     useUnifiedTopology: true
 });
 
-app.use(function(req, res) {
-    res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  });
-
 app.use(express.json());
 app.use(router);
 
