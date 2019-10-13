22ba60537725dd823c5c36957a15f4eda5aca4c1
jhonatan <jhonatan.alanf@gmail.com>
2019-10-13 16:28:16 -0300

    starting

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ let uploadConfig = require('./config/upload');
 let upload = multer(uploadConfig);
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "locahost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "locahost"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
